5466985623f94c77d74aadbfc1427afc90971e14
x3ntany <xentany@proton.me>
2024-10-15 00:33:41 -0300

    added SpecManager and null validation

--- src/main/java/me/xentany/xspec/SpecPlugin.java
+++ src/main/java/me/xentany/xspec/SpecPlugin.java
@@ -1,22 +1,36 @@
 package me.xentany.xspec;
 
+import me.xentany.xspec.api.SpecManager;
+import me.xentany.xspec.spec.SpecManagerImpl;
+import org.apache.commons.lang.Validate;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public final class SpecPlugin extends JavaPlugin {
 
-  private static SpecPlugin instance;
+  private static @MonotonicNonNull SpecPlugin instance;
+  private @MonotonicNonNull SpecManager specManager;
 
   @Override
   public void onEnable() {
-    instance = this;
+    SpecPlugin.instance = this;
+
+    this.specManager = new SpecManagerImpl();
   }
   
   @Override
   public void onDisable() {
 
   }
-  
-  public static SpecPlugin getInstance() {
-    return instance;
+
+  public static @NonNull SpecPlugin getInstance() {
+    Validate.notNull(SpecPlugin.instance, "SpecPlugin has not been initialized yet");
+    return SpecPlugin.instance;
+  }
+
+  public @NonNull SpecManager getSpecManager() {
+    Validate.notNull(this.specManager, "SpecManager has not been initialized yet");
+    return this.specManager;
   }
 }
