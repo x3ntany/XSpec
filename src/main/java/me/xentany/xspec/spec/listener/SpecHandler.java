398b782cfb0ee1925069393fdba92619300a5913
x3ntany <xentany@proton.me>
2024-10-15 15:42:06 -0300

    Added blocking of commands during spec

--- src/main/java/me/xentany/xspec/spec/listener/SpecHandler.java
+++ src/main/java/me/xentany/xspec/spec/listener/SpecHandler.java
@@ -7,16 +7,19 @@ import me.xentany.xspec.util.MessageUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
 
+import java.util.Locale;
+
 @DefaultQualifier(NonNull.class)
 public final class SpecHandler implements Listener {
 
-  private final @NonNull SpecManager specManager;
+  private final SpecManager specManager;
 
   public SpecHandler() {
     this.specManager = SpecPlugin.getInstance().getSpecManager();
@@ -39,6 +42,18 @@ public final class SpecHandler implements Listener {
     }
   }
 
+  @EventHandler
+  public void on(final PlayerCommandPreprocessEvent event) {
+    var player = event.getPlayer();
+
+    if (this.specManager.isSpectator(player) &&
+        Settings.IMP.MAIN.BLOCKED_COMMANDS.contains(event.getMessage().split(" ")[0].toLowerCase(Locale.ROOT))) {
+      event.setCancelled(true);
+      MessageUtil.formatAndSendIfNotEmpty(player, Settings.IMP.MAIN.MESSAGES.COMMAND_BLOCKED);
+    }
+  }
+
+  //todo gamemode change lol
   private void onLogout(final @NonNull Player player) {
     this.specManager.findSpec(player).ifPresent(spec -> {
       this.specManager.stop(spec);
