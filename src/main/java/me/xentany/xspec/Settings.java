9a5e9aa05b73271fb4d5f5a121de6689d1234790
x3ntany <xentany@proton.me>
2024-10-15 15:41:02 -0300

    Some changes

--- src/main/java/me/xentany/xspec/Settings.java
+++ src/main/java/me/xentany/xspec/Settings.java
@@ -3,6 +3,8 @@ package me.xentany.xspec;
 import net.elytrium.commons.config.YamlConfig;
 import net.elytrium.commons.kyori.serialization.Serializers;
 
+import java.util.List;
+
 public final class Settings extends YamlConfig {
 
   @Ignore
@@ -32,6 +34,19 @@ public final class Settings extends YamlConfig {
 
     @Comment("Maximum distance in blocks at which a spectator can fly away.")
     public double MAXIMUM_DISTANCE = 25.0d;
+    public String DATE_PATTERN = "dd.MM.yyyy - HH:mm:ss";
+
+    @Comment({
+        "Available time zones:",
+        "UTC - Coordinated Universal Time.",
+        "Europe/Moscow - Moscow Time.",
+        "America/New_York - Eastern Time (US & Canada).",
+        "Asia/Tokyo - Japan Standard Time.",
+        "For more time zones, refer to: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
+    })
+    public String TIME_ZONE = "UTC";
+
+    public List<String> BLOCKED_COMMANDS = List.of("/sethome", "/setwarp");
 
     @Create
     public MESSAGES MESSAGES;
@@ -43,12 +58,14 @@ public final class Settings extends YamlConfig {
       public String SUSPECT_NOT_FOUND = "The player could not be found.";
       public String CANNOT_SPECTATE_SELF = "You cannot spectate yourself.";
       public String NO_SPECTATE_SPECTATOR = "You cannot spectate a player who is already spectating.";
-      public String STARTED = "You are now spectating {0}.";
+      public String STARTED = "You are now spectating {0}. Date: {1}";
       public String ALREADY_STARTED = "You are already spectating someone.";
-      public String STOPPED = "You have stopped spectating.";
+      public String WORLD_NOT_FOUND = "&4 The world for teleportation could not be found. Please contact an administrator.";
+      public String STOPPED = "You have stopped spectating. Date: {0}";
       public String NOT_SPECTATING = "You are not currently spectating anyone.";
       public String SUSPECT_LEFT = "The player you were spectating has left the server.";
       public String TOO_FAR = "You cannot move too far away while spectating.";
+      public String COMMAND_BLOCKED = "You cannot use this command while spectating.";
     }
   }
 }
