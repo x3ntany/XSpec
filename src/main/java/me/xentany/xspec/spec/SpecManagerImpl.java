335dc9163df348d208c332a59e74a8df9c496c49
x3ntany <xentany@proton.me>
2024-10-15 13:00:24 -0300

    Added new api methods

--- src/main/java/me/xentany/xspec/spec/SpecManagerImpl.java
+++ src/main/java/me/xentany/xspec/spec/SpecManagerImpl.java
@@ -1,6 +1,14 @@
 package me.xentany.xspec.spec;
 
+import me.xentany.xspec.Settings;
+import me.xentany.xspec.SpecPlugin;
+import me.xentany.xspec.api.Spec;
 import me.xentany.xspec.api.SpecManager;
+import me.xentany.xspec.util.DebugInfoUtil;
+import me.xentany.xspec.util.MessageUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.Contract;
@@ -8,19 +16,36 @@ import org.jetbrains.annotations.UnmodifiableView;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class SpecManagerImpl implements SpecManager {
 
-  private final @NonNull Map<Player, SpecImpl> specs;
+  private static final double MAX_DISTANCE_SQUARED;
+
+  static {
+    MAX_DISTANCE_SQUARED = Settings.IMP.MAIN.MAXIMUM_DISTANCE * Settings.IMP.MAIN.MAXIMUM_DISTANCE;
+  }
+
+  private final @NonNull Map<Player, Spec> specs;
 
   public SpecManagerImpl() {
     this.specs = new ConcurrentHashMap<>();
-  }
 
-  @Contract(pure = true)
-  public @NonNull @UnmodifiableView Map<Player, SpecImpl> getSpecs() {
-    return Collections.unmodifiableMap(this.specs);
+    var plugin = SpecPlugin.getInstance();
+
+    Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () ->
+        specs.values().forEach(spec -> {
+          var spectator = spec.spectator();
+          var suspectLocation = spec.suspect().getLocation();
+
+          if (spectator.getLocation().distanceSquared(suspectLocation) > MAX_DISTANCE_SQUARED) {
+            Bukkit.getScheduler().runTask(plugin, () -> {
+              spectator.teleportAsync(suspectLocation);
+              MessageUtil.formatAndSendIfNotEmpty(spectator, Settings.IMP.MAIN.MESSAGES.TOO_FAR);
+            });
+          }
+        }), 60L, 60L);
   }
 
   @Override
@@ -31,7 +56,62 @@ public final class SpecManagerImpl implements SpecManager {
   }
 
   @Override
-  public boolean isSpectating(final @NonNull Player player) {
+  public boolean isSpectator(final @NonNull Player player) {
     return this.specs.containsKey(player);
+  }
+
+  @Override
+  public boolean tryStart(final @NonNull Spec spec) {
+    var spectator = spec.spectator();
+
+    if (!this.specs.containsKey(spectator)) {
+      this.specs.put(spectator, spec);
+
+      DebugInfoUtil.hideDebugInfo(spectator);
+
+      spectator.setGameMode(GameMode.SPECTATOR);
+      spectator.teleportAsync(spec.suspect().getLocation());
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public void stop(final @NonNull Spec spec) {
+    var spectator = spec.spectator();
+
+    Optional.ofNullable(Bukkit.getWorld(Settings.IMP.MAIN.TELEPORT_WORLD_NAME))
+        .ifPresentOrElse(world -> {
+          var x = Settings.IMP.MAIN.TELEPORT_X;
+          var y = Settings.IMP.MAIN.TELEPORT_Y;
+          var z = Settings.IMP.MAIN.TELEPORT_Z;
+
+          spectator.teleportAsync(new Location(world, x, y, z));
+          spectator.setGameMode(GameMode.SURVIVAL);
+
+          DebugInfoUtil.showDebugInfo(spectator);
+        }, () -> spectator.sendMessage("Teleport world not found or null"));
+
+    this.specs.remove(spectator);
+  }
+
+  @Override
+  public void stopAll() {
+    this.specs.values().forEach(this::stop);
+  }
+
+  @Override
+  public Optional<Spec> findSpec(final @NonNull Player player) {
+    return Optional.ofNullable(this.specs.get(player))
+        .or(() -> this.specs.values().stream()
+            .filter(spec -> spec.suspect().equals(player))
+            .findFirst());
+  }
+
+  @Override
+  @Contract(pure = true)
+  public @NonNull @UnmodifiableView Map<Player, Spec> getSpecs() {
+    return Collections.unmodifiableMap(this.specs);
   }
 }
