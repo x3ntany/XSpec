30bbd375d5e340ea71f34084d2176708c27246a0
x3ntany <xentany@proton.me>
2024-10-15 13:00:13 -0300

    Added new api methods

--- src/main/java/me/xentany/xspec/api/SpecManager.java
+++ src/main/java/me/xentany/xspec/api/SpecManager.java
@@ -2,9 +2,18 @@ package me.xentany.xspec.api;
 
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.UnmodifiableView;
+
+import java.util.Map;
+import java.util.Optional;
 
 public interface SpecManager {
 
   boolean isInSpec(final @NonNull Player player);
-  boolean isSpectating(final @NonNull Player player);
+  boolean isSpectator(final @NonNull Player spectator);
+  boolean tryStart(final @NonNull Spec spec);
+  void stop(final @NonNull Spec spec);
+  void stopAll();
+  Optional<Spec> findSpec(final @NonNull Player player);
+  @NonNull @UnmodifiableView Map<Player, Spec> getSpecs();
 }
