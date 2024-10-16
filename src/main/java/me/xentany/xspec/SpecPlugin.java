b15f2685828535861ddfe3c43eaaa7509568c10f
x3ntany <xentany@proton.me>
2024-10-16 02:28:04 -0300

    Some changes

--- src/main/java/me/xentany/xspec/SpecPlugin.java
+++ src/main/java/me/xentany/xspec/SpecPlugin.java
@@ -4,25 +4,27 @@ import me.xentany.xspec.api.SpecManager;
 import me.xentany.xspec.spec.command.SpecCommand;
 import me.xentany.xspec.spec.listener.SpecHandler;
 import me.xentany.xspec.spec.SpecManagerImpl;
-import org.apache.commons.lang.Validate;
+import me.xentany.xspec.util.DateFormatUtil;
+import me.xentany.xspec.util.DebugInfoUtil;
+import me.xentany.xspec.util.MessageUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.framework.qual.DefaultQualifier;
 
 import java.util.Optional;
 
-@DefaultQualifier(NonNull.class)
 public final class SpecPlugin extends JavaPlugin {
 
-  private static @MonotonicNonNull SpecPlugin instance;
-  private @MonotonicNonNull SpecManager specManager;
+  private static SpecPlugin instance;
+  private SpecManager specManager;
 
   @Override
   public void onEnable() {
     SpecPlugin.instance = this;
     Settings.IMP.reload(this.getDataFolder().toPath().resolve("config.yml").toFile());
+
+    DateFormatUtil.load();
+    DebugInfoUtil.load();
+    MessageUtil.load();
 
     this.specManager = new SpecManagerImpl();
 
@@ -30,19 +32,17 @@ public final class SpecPlugin extends JavaPlugin {
         .ifPresent(command -> command.setExecutor(new SpecCommand()));
     Bukkit.getPluginManager().registerEvents(new SpecHandler(), this);
   }
-  
+
   @Override
   public void onDisable() {
     this.specManager.stopAll();
   }
 
   public static SpecPlugin getInstance() {
-    Validate.notNull(SpecPlugin.instance, "SpecPlugin has not been initialized yet");
     return SpecPlugin.instance;
   }
 
   public SpecManager getSpecManager() {
-    Validate.notNull(this.specManager, "SpecManager has not been initialized yet");
     return this.specManager;
   }
 }
