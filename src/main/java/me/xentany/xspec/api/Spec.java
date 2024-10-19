a12bc894c7194068ba08da958921537f55d58570
x3ntany <xentany@proton.me>
2024-10-18 23:57:49 -0300

    New builder and added bossbar

--- src/main/java/me/xentany/xspec/api/Spec.java
+++ src/main/java/me/xentany/xspec/api/Spec.java
@@ -1,8 +1,10 @@
 package me.xentany.xspec.api;
 
+import me.xentany.xspec.Settings;
+import me.xentany.xspec.spec.SpecBarImpl;
 import me.xentany.xspec.spec.SpecImpl;
 import me.xentany.xspec.spec.SpecLoggerImpl;
-import org.apache.commons.lang.Validate;
+import me.xentany.xspec.util.MessageUtil;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -12,35 +14,46 @@ public interface Spec {
   Player spectator();
   Player suspect();
   SpecLogger logger();
+  SpecBar specBar();
 
-  @Contract(value = " -> new", pure = true)
-  static @NotNull Builder builder() {
-    return new Builder();
+  @Deprecated(since = "1.0.0", forRemoval = true)
+  @Contract(" -> fail")
+  static @NotNull Spec.Builder builder() {
+    throw new UnsupportedOperationException("This builder method is no longer supported. " +
+        "Use builder(Player spectator, Player suspect) instead");
+  }
+
+  @Contract("_, _ -> new")
+  static @NotNull Spec.Builder builder(final @NotNull Player spectator,
+                                       final @NotNull Player suspect) {
+    return new Spec.Builder(spectator, suspect);
   }
 
   class Builder {
 
-    private Player spectator;
-    private Player suspect;
+    private final Player spectator;
+    private final Player suspect;
+    private final SpecLogger logger;
+    private SpecBar specBar;
 
-    public @NotNull Builder spectator(final @NotNull Player spectator) {
+    public Builder(final @NotNull Player spectator, final @NotNull Player suspect) {
       this.spectator = spectator;
-      return this;
+      this.suspect = suspect;
+      this.logger = new SpecLoggerImpl(spectator, suspect);
+      this.specBar = new SpecBarImpl(MessageUtil.getFormattedComponent(Settings.IMP.MAIN.BAR_NAME, suspect.getName()), Settings.IMP.MAIN.BAR_COLOR, Settings.IMP.MAIN.BAR_OVERLAY);
     }
 
-    public @NotNull Builder suspect(final @NotNull Player suspect) {
-      this.suspect = suspect;
-      return this;
+    public void specBar(final SpecBar specBar) {
+      this.specBar = specBar;
     }
 
     public @NotNull Spec build() {
-      this.validate();
-      return new SpecImpl(this.spectator, this.suspect, new SpecLoggerImpl(this.spectator));
-    }
-
-    private void validate() {
-      Validate.notNull(this.spectator, "Spectator must be set before building");
-      Validate.notNull(this.suspect, "Suspect must be set before building");
+      return new SpecImpl(
+          this.spectator,
+          this.suspect,
+          this.logger,
+          this.specBar
+      );
     }
   }
 }
