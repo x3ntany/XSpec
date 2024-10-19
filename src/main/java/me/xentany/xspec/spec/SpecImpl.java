fbf893463be196d068a02678848527e3bd83ed58
x3ntany <xentany@proton.me>
2024-10-19 00:00:55 -0300

    Added bossbar

--- src/main/java/me/xentany/xspec/spec/SpecImpl.java
+++ src/main/java/me/xentany/xspec/spec/SpecImpl.java
@@ -1,9 +1,12 @@
 package me.xentany.xspec.spec;
 
 import me.xentany.xspec.api.Spec;
+import me.xentany.xspec.api.SpecBar;
+import me.xentany.xspec.api.SpecLogger;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public record SpecImpl(@NotNull Player spectator,
                        @NotNull Player suspect,
-                       @NotNull SpecLoggerImpl logger) implements Spec {}
+                       @NotNull SpecLogger logger,
+                       @NotNull SpecBar specBar) implements Spec {}
