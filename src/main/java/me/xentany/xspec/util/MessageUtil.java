4cbb849e0c2c8db9b6c14313225fab9d3eb2ad03
x3ntany <xentany@proton.me>
2024-10-22 16:39:49 -0300

    Changed to own optimized formatter, MessageFormat removed

--- src/main/java/me/xentany/xspec/util/MessageUtil.java
+++ src/main/java/me/xentany/xspec/util/MessageUtil.java
@@ -8,7 +8,6 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-import java.text.MessageFormat;
 import java.util.Objects;
 
 public final class MessageUtil {
@@ -33,8 +32,44 @@ public final class MessageUtil {
     }
   }
 
+
   public static @NotNull Component getFormattedComponent(final @NotNull String message,
                                                          final Object @NotNull ... args) {
-    return MessageUtil.serializer.deserialize(MessageFormat.format(message, args));
+    return MessageUtil.serializer.deserialize(MessageUtil.formatString(message, args));
+  }
+
+  private static @NotNull String formatString(final @NotNull String message,
+                                              final Object @NotNull ... args) {
+    var result = new StringBuilder();
+    int length = message.length();
+
+    for (int i = 0; i < length; i++) {
+      var currentChar = message.charAt(i);
+
+      if (currentChar == '{' && i + 1 < length) {
+        int endIndex = message.indexOf('}', i);
+
+        if (endIndex > i) {
+          var indexStr = message.substring(i + 1, endIndex);
+
+          try {
+            int argIndex = Integer.parseInt(indexStr);
+
+            if (argIndex >= 0 && argIndex < args.length) {
+              result.append(Objects.toString(args[argIndex], ""));
+            } else {
+              result.append("{").append(indexStr).append("}");
+            }
+
+            i = endIndex;
+            continue;
+          } catch (NumberFormatException ignored) {}
+        }
+      }
+
+      result.append(currentChar);
+    }
+
+    return result.toString();
   }
 }
